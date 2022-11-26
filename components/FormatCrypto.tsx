0bb9088d03a9d199d5c8371dd4f3fe8808ce68b3
Adam Gobran <adamgobes@gmail.com>
2022-11-25 20:12:11 -0400

    USDC logo

--- components/FormatCrypto.tsx
+++ components/FormatCrypto.tsx
@@ -17,7 +17,12 @@ const FormatCrypto: FC<Props> = ({
   address,
   logoWidth = 16,
 }) => {
-  const logoUrl = `${API_BASE}/redirect/currency/${address}/icon/v1`
+  let logoUrl: string
+  if (address === '0x68b7e050e6e2c7efe11439045c9d49813c1724b8') {
+    logoUrl = 'https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=023'
+  } else {
+    logoUrl = `${API_BASE}/redirect/currency/${address}/icon/v1`
+  }
 
   return (
     <FormatCurrency
